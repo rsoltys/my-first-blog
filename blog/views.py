a229c2dba20dd0802573b6dbd8b51d5a34367ec2
rsoltys <radovan.soltys@gmail.com>
2023-09-29 21:27:57 +0200

    Pridane viewy na vytvorenie/editovanie prispevkov priamo zo stranky

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.shortcuts import redirect
 from .models import Post
 from .forms import PostForm
 
